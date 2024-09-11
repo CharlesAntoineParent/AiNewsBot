3488e45563455fc7100089e3d68f406a6c70fd40
CharlesAntoineParent <charles-antoine.parent@outlook.com>
2024-09-11 22:37:48 +0000

    feat(API-KEY): Added az keyvault as the manager of api key

--- src/summarizer/chains/__init__.py
+++ src/summarizer/chains/__init__.py
@@ -1,5 +1,7 @@
 """This module implements all the chains."""
 
+from summarizer.chains.base_chain import BaseChain
+from summarizer.chains.chain_builder import ChainFactory
 from summarizer.chains.map_reduce import MapReduceChain
 
-__all__ = ["MapReduceChain"]
+__all__ = ["BaseChain", "MapReduceChain", "ChainFactory"]
